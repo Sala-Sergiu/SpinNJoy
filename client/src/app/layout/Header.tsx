7e5e4a98849e1fe8921e1f478d02ecc3eadea9cd
Sala-Sergiu <121624777+Sala-Sergiu@users.noreply.github.com>
2024-06-04 13:06:08 +0300

    Added Routing with React Router and product details page

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,15 +1,93 @@
-import { AppBar, Switch, Toolbar, Typography } from "@mui/material";
+import { ShoppingCart } from "@mui/icons-material";
+import {
+  AppBar,
+  Badge,
+  Box,
+  IconButton,
+  List,
+  ListItem,
+  Switch,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { NavLink } from "react-router-dom";
+
+const midlinks = [
+  {
+    title: "catalog",
+    path: "/catalog",
+  },
+  {
+    title: "about",
+    path: "/about",
+  },
+  {
+    title: "contact",
+    path: "/contact",
+  },
+];
+
+const rightlinks = [
+  {
+    title: "login",
+    path: "/login",
+  },
+  {
+    title: "register",
+    path: "/register",
+  },
+];
 
 interface Props {
   handleThemeChange: () => void;
 }
 
+const styles = {
+  color: "inherit",
+  textDecoration: "none",
+  typography: "h6",
+  "&:hover": {
+    color: "grey.500",
+  },
+  "&.active": {
+    color: "text.secondary",
+  },
+};
+
 export default function Header({ handleThemeChange }: Props) {
   return (
     <AppBar position="static" sx={{ marginBottom: 4 }}>
-      <Toolbar>
-        <Typography variant="h6">SpinNJoy</Typography>
-        <Switch onChange={handleThemeChange} />
+      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+        <Box>
+          <Typography variant="h5" component={NavLink} to="/" sx={styles}>
+            SpinNJoy
+          </Typography>
+          <Switch onChange={handleThemeChange} />
+        </Box>
+        <Box>
+          <List sx={{ display: "flex" }}>
+            {midlinks.map(({ title, path }) => (
+              <ListItem component={NavLink} to={path} key={path} sx={styles}>
+                {title.toUpperCase()}
+              </ListItem>
+            ))}
+          </List>
+        </Box>
+        <Box sx={{ display: "flex" }}>
+          <IconButton size="large" edge="start" color="inherit" sx={{ mr: 2 }}>
+            <Badge badgeContent={4} color="secondary">
+              <ShoppingCart />
+            </Badge>
+          </IconButton>
+
+          <List sx={{ display: "flex" }}>
+            {rightlinks.map(({ title, path }) => (
+              <ListItem component={NavLink} to={path} key={path} sx={styles}>
+                {title.toUpperCase()}
+              </ListItem>
+            ))}
+          </List>
+        </Box>
       </Toolbar>
     </AppBar>
   );
--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -9,6 +9,7 @@ import {
   Typography,
 } from "@mui/material";
 import { Product } from "../../app/layout/model/product";
+import { Link } from "react-router-dom";
 
 interface Props {
   product: Product;
@@ -48,7 +49,9 @@ export default function ProductCard({ product }: Props) {
       </CardContent>
       <CardActions>
         <Button size="small">ADD TO CART</Button>
-        <Button size="small">VIEW</Button>
+        <Button component={Link} to={`/catalog/${product.id}`} size="small">
+          VIEW
+        </Button>
       </CardActions>
     </Card>
   );
