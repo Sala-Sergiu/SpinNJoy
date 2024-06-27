bd1f5ab3cdc509ec2afcc2a9084ade2504aef0d0
Sala-Sergiu <121624777+Sala-Sergiu@users.noreply.github.com>
2024-06-27 19:29:37 +0300

    test

---
+++ client/src/app/router/Routes.tsx
@@ -0,0 +1,82 @@
+import { Navigate, createBrowserRouter } from "react-router-dom";
+import App from "../layout/App";
+import Catalog from "../../features/catalog/Catalog";
+import AboutPage from "../../features/about/AboutPage";
+import ContactPage from "../../features/contact/ContactPage";
+import HomePage from "../../features/home/HomePage";
+import ProductDetails from "../../features/catalog/ProductDetails";
+import ServerError from "../errors/ServerError";
+import NotFound from "../errors/NotFound";
+import BasketPage from "../../features/basket/BasketPage";
+import Login from "../../features/account/Login";
+import Register from "../../features/account/Register";
+import RequireAuth from "./RequireAuth";
+import Orders from "../../features/orders/Orders";
+import CheckOutWrapper from "../../features/checkout/CheckOutWrapper";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        element: <RequireAuth />,
+        children: [
+          {
+            path: "checkout",
+            element: <CheckOutWrapper />,
+          },
+          {
+            path: "orders",
+            element: <Orders />,
+          },
+        ],
+      },
+      {
+        path: "",
+        element: <HomePage />,
+      },
+      {
+        path: "catalog",
+        element: <Catalog />,
+      },
+      {
+        path: "catalog/:id",
+        element: <ProductDetails />,
+      },
+      {
+        path: "about",
+        element: <AboutPage />,
+      },
+      {
+        path: "contact",
+        element: <ContactPage />,
+      },
+      {
+        path: "server-error",
+        element: <ServerError />,
+      },
+      {
+        path: "not-found",
+        element: <NotFound />,
+      },
+      {
+        path: "basket",
+        element: <BasketPage />,
+      },
+
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+      },
+      {
+        path: "*",
+        element: <Navigate replace to="not-found" />,
+      },
+    ],
+  },
+]);
